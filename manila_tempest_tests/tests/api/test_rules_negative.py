de1ba7df7614cd7fa44071c2c856e6ce095de70c
Maurice Schreiber <maurice.schreiber@sap.com>
2018-08-27 17:48:36 +0200

    give share type tempest prefix
    Change-Id: I9b3a55e73702ec67018f3dc5896db6393e79d3f4
    (cherry picked from commit 936156751531f49ec389a71b3c7aa52d7dad83a3)

--- manila_tempest_tests/tests/api/test_rules_negative.py
+++ manila_tempest_tests/tests/api/test_rules_negative.py
@@ -157,7 +157,7 @@ class ShareIpRulesForNFSNegativeTest(base.BaseSharesMixedTest):
             self.protocol)
         extra_specs = self.add_extra_specs_to_dict(
             {"share_backend_name": 'invalid_backend'})
-        share_type = self.create_share_type('invalid_backend',
+        share_type = self.create_share_type('tempest_invalid_backend',
                                             extra_specs=extra_specs,
                                             client=self.admin_client,
                                             cleanup_in_class=False)
