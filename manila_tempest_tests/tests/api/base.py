4cd24a9079551ead1eb9effcd4f78d21f6219f92
Dmitry Galkin <galkindmitrii@gmail.com>
2021-12-22 14:33:29 +0100

    Force the share type to be netapp:thin_provisioned : true
    - this is recommended by netapp and used in prod
    - also thick provisioning does not work with logical space reporting
    
    (cherry picked from commit 4383a3166c435d169c2ff07deeb7a2290106d364)

--- manila_tempest_tests/tests/api/base.py
+++ manila_tempest_tests/tests/api/base.py
@@ -1042,6 +1042,8 @@ class BaseSharesAdminTest(BaseSharesTest):
             extra_specs_dict.update(extra_specs)
         if CONF.share.capability_thin_provisioned:
             extra_specs_dict['thin_provisioning'] = 'True'
+
+        extra_specs_dict.update({"netapp:thin_provisioned" : 'True'})
         return extra_specs_dict
 
     @classmethod
